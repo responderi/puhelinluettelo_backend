d45d491c9e0ca54608c63ef1e7e259148635b0f7
responderi <rv.mahti@gmail.com>
2018-10-29 23:06:05 +0200

    Added eslint

--- index.js
+++ index.js
@@ -12,102 +12,102 @@ morgan.token('body', req => JSON.stringify(req.body))
 app.use(morgan(':method :url :body :status :res[content-length] - :response-time ms'))
 
 app.get('/api/persons', (req, res) => {
-    Person
-        .find({})
-        .then(persons => {
-            res.json(persons.map(Person.format))
-        })
+  Person
+    .find({})
+    .then(persons => {
+      res.json(persons.map(Person.format))
+    })
 })
 
 app.get('/api/persons/:id', (req, res) => {
-    Person
-        .findById(req.params.id)
-        .then(person => {
-            if(person) {
-                res.json(Person.format(person))
-            } else {
-                res.status(404).end()
-            }
-        })
-        .catch(error => {
-            console.log(error)
-            res.status(400).send({ error: 'malformatted id' })
-        })
+  Person
+    .findById(req.params.id)
+    .then(person => {
+      if(person) {
+        res.json(Person.format(person))
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(400).send({ error: 'malformatted id' })
+    })
 })
 
 app.delete('/api/persons/:id', (req, res) => {
-    Person
-        .findByIdAndRemove(req.params.id)
-        .then(result => {
-            res.status(204).end()
-        })
-        .catch(error => {
-            res.status(400).send({ error: 'malformatted id' })
-        })
+  Person
+    .findByIdAndRemove(req.params.id)
+    .then(result => {
+      res.status(204).end()
+    })
+    .catch(error => {
+      res.status(400).send({ error: 'malformatted id' })
+    })
 })
 
 app.put('api/persons/:id', (req, res) => {
-    const body = req.body
-    const person = {
-        name: body.name,
-        number: body.number
-    }
+  const body = req.body
+  const person = {
+    name: body.name,
+    number: body.number
+  }
 
-    Person
-        .findByIdAndUpdate(req.params.id, person)
-        .then(changePerson => {
-            res.json(Person.format(changePerson))
-        })
-        .catch(error => {
-            res.status(400).send({ error: 'malformatted id' })
-        })
+  Person
+    .findByIdAndUpdate(req.params.id, person)
+    .then(changePerson => {
+      res.json(Person.format(changePerson))
+    })
+    .catch(error => {
+      res.status(400).send({ error: 'malformatted id' })
+    })
 })
 
 app.post('/api/persons', (req, res) => {
-    const body = req.body
-    
-    if(body.name === undefined) {
-        return res.status(400).json({ error: 'name is missing' })
-    }
+  const body = req.body
 
-    if(body.number === undefined) {
-        return res.status(400).json({ error: 'number is missing' })
-    }
+  if(body.name === undefined) {
+    return res.status(400).json({ error: 'name is missing' })
+  }
 
-    const person = new Person({
-        name: body.name,
-        number: body.number,
-        id: Math.floor(Math.random() * Math.floor(100000))
+  if(body.number === undefined) {
+    return res.status(400).json({ error: 'number is missing' })
+  }
+
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+    id: Math.floor(Math.random() * Math.floor(100000))
+  })
+
+  Person
+    .find({ name: person.name })
+    .then(result => {
+      if(!(result.length <= 0)) {
+        res.status(400).send({ error: 'give name already exists' })
+      } else {
+        person
+          .save()
+          .then(newPerson => {
+            res.json(Person.format(newPerson))
+          })
+      }
     })
-
-    Person
-        .find({ name: person.name })
-        .then(result => {
-            if(!(result.length <= 0)) {
-                res.status(400).send({ error: 'give name already exists' })
-            } else {
-                person
-                    .save()
-                    .then(newPerson => {
-                        res.json(Person.format(newPerson))
-                    })
-            }
-        })
 })
 
 app.get('/info', (req, res) => {
-    Person
-        .find({})
-        .then(persons => {
-            res.send(
-                `<p>puhelinluettelossa on ${persons.length} henkilön tiedot</p>
-                <p>${new Date()}</p>
-                `
-            )
-        })
+  Person
+    .find({})
+    .then(persons => {
+      res.send(
+        `<p>puhelinluettelossa on ${persons.length} henkilön tiedot</p>
+        <p>${new Date()}</p>
+        `
+      )
+    })
 })
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-    console.log(`Päristelee portissa ${PORT}`)
+  console.log(`Päristelee portissa ${PORT}`)
 })
--- models/person.js
+++ models/person.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
+if (process.env.NODE_ENV !== 'production'){
+  require('dotenv').config()
 }
 
 const url = process.env.MONGODB_URI
@@ -9,17 +9,17 @@ const url = process.env.MONGODB_URI
 mongoose.connect(url)
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
-    id: String
+  name: String,
+  number: String,
+  id: String
 })
 
 personSchema.statics.format = (person) => {
-    return {
-        name: person.name,
-        number: person.number,
-        id: person._id
-    }
+  return {
+    name: person.name,
+    number: person.number,
+    id: person._id
+  }
 }
 
 const Person = mongoose.model('Person', personSchema)
